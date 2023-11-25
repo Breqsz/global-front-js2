936324358a2a110db8a717d9d2420b3914730ce0
Breqsz <rm97974@fiap.com>
2023-11-24 22:12:32 -0300

    Main e App atualizados

---
+++ src/main.jsx
@@ -0,0 +1,27 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+//BLOCO DAS ROTAS
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routes/Home/Home.jsx';
+import Login from './routes/Login/Login.jsx';
+import Erro404 from './routes/Erro404.jsx';
+import Cadastro from './routes/Cadastro/Cadastro.jsx';
+//BLOCO DAS ROTAS
+
+const router = createBrowserRouter([
+  {
+    path: "/", element: <App/>, errorElement: <Erro404 />,
+    children: [
+      { path: "/", element: <Home/> },
+      { path: "/login", element: <Login/> },
+      { path: "/cadastrar", element: <Cadastro /> },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <RouterProvider router={router} />
+
+)
